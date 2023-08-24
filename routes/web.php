8a2465f870de6210014136e0324148417aaa8a3e
JiaLe <jialeee0511@gmail.com>
2023-08-24 22:52:50 +0800

    Unauthenticated user will be redirected to Login page instead of Welcome page

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,12 @@ use App\Http\Controllers\MemberController;
 
 Route::webhooks('webhooks');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
